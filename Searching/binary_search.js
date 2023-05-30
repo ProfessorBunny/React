0b1079512ed36390816481a1c607c378e92203e2
ProfessorBunny <nikunjraghav24@gmail.com>
2023-05-30 21:05:00 +0100

    Searching Algo Practice

--- Searching/binary_search.js
+++ Searching/binary_search.js
@@ -1,21 +1,21 @@
 //Binary Search is Only possible with sorted array
-function binarySearch(arr, elem) {
-  var start = 0;
-  var end = arr.length - 1;
-  var middle = Math.floor((start + end) / 2);
-  while (arr[middle] !== elem && start <= end) {
-    if (elem < arr[middle]) {
-      end = middle - 1;
-    } else {
-      start = middle + 1;
-    }
-    middle = Math.floor((start + end) / 2);
-  }
-  if (arr[middle] === elem) {
-    return middle;
-  }
-  return -1;
-}
+// function binarySearch(arr, elem) {
+//   var start = 0;
+//   var end = arr.length - 1;
+//   var middle = Math.floor((start + end) / 2);
+//   while (arr[middle] !== elem && start <= end) {
+//     if (elem < arr[middle]) {
+//       end = middle - 1;
+//     } else {
+//       start = middle + 1;
+//     }
+//     middle = Math.floor((start + end) / 2);
+//   }
+//   if (arr[middle] === elem) {
+//     return middle;
+//   }
+//   return -1;
+// }
 
 // // Refactored Version
 // function binarySearch(arr, elem) {
@@ -30,4 +30,21 @@ function binarySearch(arr, elem) {
 //   return arr[middle] === elem ? middle : -1;
 // }
 
-console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 50));
+const binarySearch = (numbers, target) => {
+  let start = 0;
+  let end = numbers.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  while (numbers[middle] !== target && start <= end) {
+    if (numbers[middle] < target) {
+      start = middle + 1;
+    } else end = middle - 1;
+    middle = Math.floor(start + end / 2);
+  }
+
+  if (numbers[middle] === target) {
+    return middle;
+  } else return -1;
+};
+
+console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 30));
--- Searching/linear_search.js
+++ Searching/linear_search.js
@@ -1,8 +1,19 @@
-linearSearch = (numbers, target) => {
+// linearSearch = (numbers, target) => {
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] === target) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// };
+
+// let value = linearSearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 99);
+
+// console.log(value);
+
+const linearSearch = (numbers, target) => {
   for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] === target) {
-      return i;
-    }
+    if (numbers[i] === target) return i;
   }
   return -1;
 };
